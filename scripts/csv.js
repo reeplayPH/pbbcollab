dd75e19293fd86cc50a037a6b845604a04a9ff70
reeplayPH <reel.and.play@gmail.com>
2025-04-15 17:53:15 +0800

    reeplay

--- scripts/csv.js
+++ scripts/csv.js
@@ -6,7 +6,7 @@ var CSV = {};
   "use strict";
   my.__type__ = "csv";
 
-  // use either jQuery or Underscore Deferred depending on what is available
+  // Use either jQuery or Underscore Deferred depending on what is available
   var Deferred =
     (typeof jQuery !== "undefined" && jQuery.Deferred) ||
     (typeof _ !== "undefined" && _.Deferred) ||
