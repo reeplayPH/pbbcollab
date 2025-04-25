429f77703f9af9ff147d4ffe52d1e4cc0bc9467b
reeplayPH <reel.and.play@gmail.com>
2025-04-25 20:42:05 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -65,7 +65,7 @@ function toggleMenu() {
 }
 
 // Optional: Close the menu if clicked outside
-window.onclick = function(event) {
+/*window.onclick = function(event) {
   if (!event.target.matches('.display-options-icon')) {
     var menus = document.getElementsByClassName('click-menu');
     for (var i = 0; i < menus.length; i++) {
@@ -75,7 +75,7 @@ window.onclick = function(event) {
       }
     }
   }
-}
+}*/
 // Takes in an array of trainees and converts it to js objects
 // Follows this schema:
 /*
