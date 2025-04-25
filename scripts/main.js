9e6ed227da579af967e83227d27cd0391e3a1c39
reeplayPH <reel.and.play@gmail.com>
2025-04-25 11:37:02 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -405,7 +405,7 @@ var trainees = [];
 var filteredTrainees = [];
 // holds the ordered list of rankings that the user selects
 var ranking = newRanking();
-const rowNums = [1,2,3];
+const rowNums = [1,2,3,4];
 //window.addEventListener("load", function () {
   populateRanking();
   readFromCSV("./housemate_info.csv");
