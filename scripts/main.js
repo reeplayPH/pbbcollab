823ad33c43ab1e6e99c2abc71b97af0b03205afe
reeplayPH <reel.and.play@gmail.com>
2025-04-28 10:11:53 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -55,14 +55,27 @@ function getRanking() {
   }
 }
 
-function toggleMenu() {
+window.onload = function() {
+    document.getElementById('clickMenu').style.display = 'none'; // Ensure menu is hidden on page load
+
+    document.getElementById('.display-options-icon').addEventListener('click', function() {
+        var menu = document.getElementById('clickMenu');
+        if (menu.style.display === 'none') {
+            menu.style.display = 'block';
+        } else {
+            menu.style.display = 'none';
+        }
+    });
+};
+
+/*function toggleMenu() {
   var menu = document.getElementById('clickMenu');
   if (menu.style.display === 'block') {
     menu.style.display = 'none';
   } else {
     menu.style.display = 'block';
   }
-}
+}*/
 
 // Optional: Close the menu if clicked outside
 /*window.onclick = function(event) {
