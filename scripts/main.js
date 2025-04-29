b49d460bf1e99288716ca3638af2847c7b7390de
reeplayPH <reel.and.play@gmail.com>
2025-04-29 23:20:31 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -87,7 +87,7 @@ function toggleMenu() {
 }
 
 // Optional: Close the menu if clicked outside
-/*window.onclick = function(event) {
+window.onclick = function(event) {
    (!event.target.matches('.display-options-icon')) {
     var menus = document.getElementsByClassName('click-menu');
     for (var i = 0; i < menus.length; i++) {
@@ -97,7 +97,7 @@ function toggleMenu() {
       }
     }
   }
-}*/
+}
 
 function convertCSVArrayToTraineeData(csvArrays) {
   trainees = csvArrays.map(function(traineeArray, index) {
